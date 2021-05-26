7cd83aa7541af9a8c2f1aaf959b7fe05070a73d5
sailandsaint <andreas.sailer@web.de>
2021-05-26 12:55:40 +0000

    initial setup

---
+++ IterativeSolution/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    int fi = 0, second = 1;
+
+    for (int i = 0; i < n; i++) {
+        int swap = fi;
+        fi = second;
+        second += swap;
+    }
+
+    return fi;
+}
+
+
+int main() {
+    printf("%d", fibonacci(0));
+    printf("%d", fibonacci(1));
+    printf("%d", fibonacci(2));
+    printf("%d", fibonacci(3));
+    printf("%d", fibonacci(4));
+    printf("%d", fibonacci(5));
+    printf("%d", fibonacci(6));
+
+    return 0;
+}
---
+++ RecursiveSolution/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    int i = 0;
+    int a = i;
+    if(0 == n) {
+        return 0;
+    } else if(1 == n) {
+        return 1;
+    } else {
+        return fibonacci(n-2) + fibonacci(n-1);
+    }
+}
+
+int main() {
+    printf("%d", fibonacci(0));
+    printf("%d", fibonacci(1));
+    printf("%d", fibonacci(2));
+    printf("%d", fibonacci(3));
+    printf("%d", fibonacci(4));
+    printf("%d", fibonacci(5));
+    printf("%d", fibonacci(6));
+
+    return 0;
+}
